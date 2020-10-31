ab6bc92e3a25243802bc88e882b5915577bd13f2
grantposell <grantposell@gmail.com>
2020-10-30 22:36:19 -0500

    Added a Signup2 option

---
+++ src/pages/SignUp2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./SignUp2.css"
+
+
+const SignUp2 = () => {
+    return (
+        <div id="login-box">
+            <div class="left">
+                <h1>Sign up</h1>
+
+                <input type="text" name="username" placeholder="Username" />
+                <input type="text" name="email" placeholder="E-mail" />
+                <input type="password" name="password" placeholder="Password" />
+                <input type="password" name="password2" placeholder="Retype password" />
+
+                <input type="submit" name="signup_submit" value="Sign me up" />
+            </div>
+
+            <div class="right">
+                <h1>Sign in</h1>
+                <br />
+                <button class="social-signin email">Log in with Email</button>
+                <button class="social-signin google">Log in with Google</button>
+                <button class="social-signin microsoft">Log in with Microsoft</button>
+            </div>
+            <div class="or">OR</div>
+        </div>
+    );
+};
+export default SignUp2;
--- src/pages/Tab1.js
+++ src/pages/Tab1.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+// import SignUp from './SignUp';
+import SignUp2 from './SignUp2';
 
 const Tab1 = () => {
   return (
@@ -12,12 +13,13 @@ const Tab1 = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <SignUp2/>
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Listings</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="Tab 1 page" /> */}
       </IonContent>
     </IonPage>
   );
