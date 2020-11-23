549d44ea212413024bdda1c137650c417cf6332c
grantposell <grantposell@gmail.com>
2020-11-22 20:39:47 -0600

    Added delete and add icons on User Permissions

--- src/pages/UserAdmin.js
+++ src/pages/UserAdmin.js
@@ -1,68 +1,91 @@
 import React from 'react';
 import "./SignUp2.css";
-import { IonList, IonItem, IonLabel, IonAvatar, IonIcon, IonSelect, IonSelectOption } from '@ionic/react';
-import { personCircleOutline } from 'ionicons/icons';
+import { IonList, IonItem, IonLabel, IonAvatar, IonIcon, IonCard } from '@ionic/react';
+import { personCircleOutline, closeCircleOutline, addCircleOutline } from 'ionicons/icons';
 
 const UserAdmin = () => {
     return (
         <div>
-            <IonList style={{ width: 600, }}>
+            <h2>User Permissions</h2>
+            <br />
+            <IonCard style={{ width: 600, position: 'relative' }}>
                 <IonItem>
                     <IonAvatar slot="start">
                         <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
                     </IonAvatar>
-                    <IonLabel>James Dean</IonLabel>
+                    <ion-input placeholder="Full Name"></ion-input>
+                    <ion-input placeholder="Email"></ion-input>
                     <ion-select slot='end' value="Viewer">
                         <ion-select-option value="Viewer">Viewer</ion-select-option>
                         <ion-select-option value="User">User</ion-select-option>
                         <ion-select-option value="Admin">Admin</ion-select-option>
                     </ion-select>
+                    <IonIcon slot='end' icon={addCircleOutline} />
                 </IonItem>
-                <IonItem>
-                    <IonAvatar slot="start">
-                        <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
-                    </IonAvatar>
-                    <IonLabel>Marion Cotillard</IonLabel>
-                    <ion-select slot='end' value="Viewer">
-                        <ion-select-option value="Viewer">Viewer</ion-select-option>
-                        <ion-select-option value="User">User</ion-select-option>
-                        <ion-select-option value="Admin">Admin</ion-select-option>
-                    </ion-select>
-                </IonItem>
-                <IonItem>
-                    <IonAvatar slot="start">
-                        <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
-                    </IonAvatar>
-                    <IonLabel>Ricky Martin</IonLabel>
-                    <ion-select slot='end' value="Viewer">
-                        <ion-select-option value="Viewer">Viewer</ion-select-option>
-                        <ion-select-option value="User">User</ion-select-option>
-                        <ion-select-option value="Admin">Admin</ion-select-option>
-                    </ion-select>
-                </IonItem>
-                <IonItem>
-                    <IonAvatar slot="start">
-                        <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
-                    </IonAvatar>
-                    <IonLabel>Willie Nelson</IonLabel>
-                    <ion-select slot='end' value="Viewer">
-                        <ion-select-option value="Viewer">Viewer</ion-select-option>
-                        <ion-select-option value="User">User</ion-select-option>
-                        <ion-select-option value="Admin">Admin</ion-select-option>
-                    </ion-select>
-                </IonItem>
-                <IonItem>
-                    <IonAvatar slot="start">
-                        <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
-                    </IonAvatar>
-                    <IonLabel>Chuck Norris</IonLabel>
-                    <ion-select slot='end' value="Viewer">
-                        <ion-select-option value="Viewer">Viewer</ion-select-option>
-                        <ion-select-option value="User">User</ion-select-option>
-                        <ion-select-option value="Admin">Admin</ion-select-option>
-                    </ion-select>
-                </IonItem>
-            </IonList>
+                <br/>
+                <IonList >
+                    <IonItem>
+                        <IonAvatar slot="start">
+                            <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
+                        </IonAvatar>
+                        <IonLabel>James Dean</IonLabel>
+                        <ion-select slot='end' value="Viewer">
+                            <ion-select-option value="Viewer">Viewer</ion-select-option>
+                            <ion-select-option value="User">User</ion-select-option>
+                            <ion-select-option value="Admin">Admin</ion-select-option>
+                        </ion-select>
+                        <IonIcon slot='end' icon={closeCircleOutline} />
+                    </IonItem>
+                    <IonItem>
+                        <IonAvatar slot="start">
+                            <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
+                        </IonAvatar>
+                        <IonLabel>Marion Cotillard</IonLabel>
+                        <ion-select slot='end' value="Viewer">
+                            <ion-select-option value="Viewer">Viewer</ion-select-option>
+                            <ion-select-option value="User">User</ion-select-option>
+                            <ion-select-option value="Admin">Admin</ion-select-option>
+                        </ion-select>
+                        <IonIcon slot='end' icon={closeCircleOutline} />
+                    </IonItem>
+                    <IonItem>
+                        <IonAvatar slot="start">
+                            <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
+                        </IonAvatar>
+                        <IonLabel>Ricky Martin</IonLabel>
+                        <ion-select slot='end' value="Viewer">
+                            <ion-select-option value="Viewer">Viewer</ion-select-option>
+                            <ion-select-option value="User">User</ion-select-option>
+                            <ion-select-option value="Admin">Admin</ion-select-option>
+                        </ion-select>
+                        <IonIcon slot='end' icon={closeCircleOutline} />
+                    </IonItem>
+                    <IonItem>
+                        <IonAvatar slot="start">
+                            <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
+                        </IonAvatar>
+                        <IonLabel>Willie Nelson</IonLabel>
+                        <ion-select slot='end' value="Viewer">
+                            <ion-select-option value="Viewer">Viewer</ion-select-option>
+                            <ion-select-option value="User">User</ion-select-option>
+                            <ion-select-option value="Admin">Admin</ion-select-option>
+                        </ion-select>
+                        <IonIcon slot='end' icon={closeCircleOutline} />
+                    </IonItem>
+                    <IonItem>
+                        <IonAvatar slot="start">
+                            <IonIcon style={{ fontSize: '35px' }} icon={personCircleOutline} />
+                        </IonAvatar>
+                        <IonLabel>Chuck Norris</IonLabel>
+                        <ion-select slot='end' value="Viewer">
+                            <ion-select-option value="Viewer">Viewer</ion-select-option>
+                            <ion-select-option value="User">User</ion-select-option>
+                            <ion-select-option value="Admin">Admin</ion-select-option>
+                        </ion-select>
+                        <IonIcon slot='end' icon={closeCircleOutline} />
+                    </IonItem>
+                </IonList>
+            </IonCard>
         </div>
     );
 };
