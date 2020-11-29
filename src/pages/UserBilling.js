6c1d5cdf4cd86c8563e0ab2c975c58c8c3fd07e9
grantposell <grantposell@gmail.com>
2020-11-28 21:58:10 -0600

    Added user membership descriptions

--- src/pages/UserBilling.js
+++ src/pages/UserBilling.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import { Card, CardTitle, CardText, CardActions, Button, List, ListItem } from 'react-mdl';
 import "./SignUp2.css"
 
 
@@ -9,30 +9,35 @@ const UserBilling = () => {
             <div className="project-card">
                 {/* Project 1 */}
                 <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(/assets/images/cirrus.jpg) center / cover'}}>Nimbus</CardTitle>
+                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(/assets/images/cirrus.jpg) center / cover' }}>Nimbus</CardTitle>
                     <CardText>
-                        <li>
-                            <ul></ul>
-                        </li>
+                        <List>
+                            <ListItem>$10 User/Month</ListItem>
+                            <ListItem>Unlimited Free Viewers</ListItem>
+                            <ListItem>Unlimited Products &amp; Listings</ListItem>
+                        </List>
                     </CardText>
                     <CardActions border>
                         <a href="/">
-                            <Button raised colored style={{ float: 'right'}}>Select</Button></a>
+                            <Button raised colored style={{ float: 'right' }}>Select</Button></a>
                     </CardActions>
                 </Card>
             </div>
             <div className="project-card">
                 {/* Project 2 */}
                 <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(/assets/images/nimbus.jpg) center / cover'}}>Cirrus</CardTitle>
+                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(/assets/images/nimbus.jpg) center / cover' }}>Cirrus</CardTitle>
                     <CardText>
-                        <li>
-                            <ul></ul>
-                        </li>
+                        <List>
+                            <ListItem>$15 User/Month</ListItem>
+                            <ListItem>Unlimited Free Viewers</ListItem>
+                            <ListItem>Unlimited Products &amp; Listings</ListItem>
+                            <ListItem>User Report Dashboard</ListItem>
+                        </List>
                     </CardText>
                     <CardActions border>
                         <a href="/">
-                            <Button raised colored style={{ float: 'right'}}>Select</Button>
+                            <Button raised colored style={{ float: 'right' }}>Select</Button>
                         </a>
                     </CardActions>
                 </Card>
@@ -40,15 +45,19 @@ const UserBilling = () => {
             <div className="project-card">
                 {/* Project 3 */}
                 <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(/assets/images/stratus.jpg) center / cover'}}>Stratus</CardTitle>
+                    <CardTitle style={{ color: '#fff', height: '176px', background: 'url(/assets/images/stratus.jpg) center / cover' }}>Stratus</CardTitle>
                     <CardText>
-                        <li>
-                            <ul></ul>
-                        </li>
+                    <List>
+                            <ListItem>$20 User/Month</ListItem>
+                            <ListItem>Unlimited Free Viewers</ListItem>
+                            <ListItem>Unlimited Products &amp; Listings</ListItem>
+                            <ListItem>User Report Dashboard</ListItem>
+                            <ListItem>REST API Access</ListItem>
+                        </List>
                     </CardText>
                     <CardActions border>
                         <a href="/">
-                            <Button raised colored style={{ float: 'right'}}>Select</Button>
+                            <Button raised colored style={{ float: 'right' }}>Select</Button>
                         </a>
                     </CardActions>
                 </Card>
